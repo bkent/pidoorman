ec9b43b7c2f622c9551fad98f65b4b26c2634fd0
Ben Kent <mr.bkent@gmail.com>
2014-06-04 23:17:58 +0100

    Optimise login for mobile

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+<title>Pidoorman Login</title>
+<meta name="HandheldFriendly" content="true" />
+<meta name="MobileOptimized" content="320" />
+<meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, user-scaleable=no, width=device-width" />
 <link rel="icon" href="./images/favicon.ico" type="image/x-icon" />
 <link rel="shortcut icon" href="./images/favicon.ico" type="image/x-icon" />
 <link rel="stylesheet" href="./css/pidoorman.css">
@@ -38,10 +42,9 @@ $(document).ready(function() {
 	
 });
 </script>
-<title>PiDoorMan Access Control</title>
 </head>
 <body OnLoad="document.form.user_name.focus();"><font face="arial">
-<br /><br /><br /><br /><br /><table width="300" border="2" align="center" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
+<br /><br /><br /><br /><br /><table width="25%" border="2" align="center" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
 <tr>
 <form name="form" id="form" method="post" action="login.php">
 <td>
@@ -50,14 +53,14 @@ $(document).ready(function() {
 <td colspan="3" align="center" ><a href="../"><img src="./images/pidoorman.png" alt="PiDoorMan" title="PiDoorMan" height="100px" /></a></td>
 </tr>
 <tr>
-<td width="78"><b>Username</b></td>
-<td width="6">:</td>
-<td width="294"><input id="user_name" name="user_name" size="40%" /></td>
+<td width="25%"><b>Username</b></td>
+<td width="5%">:</td>
+<td width="50%"><input id="user_name" name="user_name" /></td>
 </tr>
 <tr>
 <td><b>Password</b></td>
 <td>:</td>
-<td><input type="password" name="password" id="password" size="40%" /></td>
+<td><input type="password" name="password" id="password" /></td>
 </tr>
 <tr>
 <td colspan="2"><i><font color="#C0C0C0">v 1.0</font></i></td>
